8373ee844357eb2ba1859a8f7b7048d1549fbc4b
Brady Richmond <brady@bradymrichmond.com>
2023-03-26 19:47:19 -0700

    fix cognito context bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import {
   createBrowserRouter,
   Navigate,
   RouterProvider,
+  useNavigate,
 } from "react-router-dom";
 import Clients from './features/clients';
 import { Amplify } from 'aws-amplify';
@@ -32,13 +33,15 @@ const buildRoutes = (isLoggedIn: boolean, pathname: string, userGroups: string[]
   }
 ]);
 
-export const CognitoContext = createContext({ userIsLoggedIn: false, userGroups: [] });
+export const CognitoContext = createContext({ userIsLoggedIn: false, setUserIsLoggedIn: (loggedIn: boolean) => {}, userGroups: [], setUserGroups: (groups: []) => {} });
 
 const App = () => {
   const pathname = window.location.pathname;
+  const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
+  const [userGroups, setUserGroups] = useState([]);
 
   return (
-    <CognitoContext.Provider value={{userIsLoggedIn: false, userGroups: []}}>
+    <CognitoContext.Provider value={{userIsLoggedIn, setUserIsLoggedIn, userGroups, setUserGroups}}>
       <CognitoContext.Consumer>
         { value => 
           <RouterProvider router={buildRoutes(value.userIsLoggedIn, pathname, value.userGroups)} />
@@ -49,12 +52,18 @@ const App = () => {
 }
 
 const Login = () => {
+  const navigate = useNavigate();
+  const cognito = useContext(CognitoContext);
+
   const userLoggedIn = (user: any) => {
+    cognito.setUserIsLoggedIn(true);
+    cognito.setUserGroups(user.signInUserSession.accessToken.payload['cognito:groups']);
+    navigate('/')
     return true;
   }
 
   return (
-    <Authenticator hideSignUp>
+    <Authenticator>
       {({ signOut, user }) => (
         <Box width='auto' height='100%'>
           {user && userLoggedIn(user) && 
