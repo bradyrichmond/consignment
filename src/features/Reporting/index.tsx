0f8da4a7d252484f91a6b4df05269cd090a94d43
Brady Richmond <brady@bradymrichmond.com>
2023-08-01 20:36:27 -0700

    initial hourly reporting

--- src/features/Reporting/index.tsx
+++ src/features/Reporting/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import EndOfDayReport from './EndOfDayReport';
+import HourlySales from './HourlySales';
 
 const REPORTS: string[] = ['End of day'];
 
@@ -19,13 +20,19 @@ const Reporting = () => {
 
     return (
         <Box padding='2rem' display='flex' flexDirection='column' flexWrap='wrap'>
-            <Box display='flex' flexDirection='row'>
-                {REPORTS.map((report: string) => <ReportButton key={report} label={report} handleClick={handleClick} />)}
+            <Box display='flex' flexDirection='column'>
+                <Typography>Printable Reports</Typography>
+                <Box display='flex' flexDirection='row'>
+                    {REPORTS.map((report: string) => <ReportButton key={report} label={report} handleClick={handleClick} />)}
+                </Box>
             </Box>
             <Box flex='1'>
                 {isShowingEndOfDayPreview && 
                     <EndOfDayReport />
                 }
+            </Box>
+            <Box display='flex' flexDirection='column'>
+                <HourlySales />
             </Box>
         </Box>
     )
